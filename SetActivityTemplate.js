c794604388ea614b0812d8d98e0d4910e2233847
Martix <66840932+MartixInTheMatrix@users.noreply.github.com>
2020-11-19 23:07:54 +0100

    Create SetActivityTemplate.js

---
+++ SetActivityTemplate.js
@@ -0,0 +1,19 @@
+ const activities_list = [
+    `Bot Utilities Devs Center`,
+    `Dev By ⚡̶̔͑Z̷͆̀ê̵͝u̶̽͘ṡ̴̔⚡̷͌̆#0069`,
+    `</>help`
+  ];
+  
+  dr.on("ready", () => {
+    setInterval(() => {
+      const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
+      dr.user.setActivity(activities_list[index], {
+        status: "online",
+        type: "STREAMING",
+        url: "https://twitch.tv/dubrin"
+      });
+    }, 2000);
+    console.log(
+      `${dr.user.username} connecté ${dr.users.cache.size} utilisateurs !`
+    );
+  }); 
